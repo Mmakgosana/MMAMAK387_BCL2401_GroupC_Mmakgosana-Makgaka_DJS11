ca0b7e3b0c985f1d39e7892825bbf13137e6c6f1
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-21 10:34:04 +0200

    Add data fetching and error handling in Series component with show more functionality

--- src/pages/Series/Series.jsx
+++ src/pages/Series/Series.jsx
@@ -6,3 +6,19 @@ export default function Series() {
     const [series, setSeries] = useState([]);
     const [error, setError] = useState(null);
     const [visibleCount, setVisibleCount] = useState(12);
+
+    useEffect(() => {
+        fetch("https://podcast-api.netlify.app")
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => setSeries(data))
+          .catch((error) => setError(error));
+      }, []);
+    
+      const showMoreSeries = () => {
+        setVisibleCount((prevCount) => prevCount + 9);
+      };   
