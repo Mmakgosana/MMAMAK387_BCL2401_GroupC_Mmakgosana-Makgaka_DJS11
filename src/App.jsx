5dd6c848d3762cbacda24995226b3c932ec7a3e2
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-21 11:03:29 +0200

    Configure routing for home, about, series, series details and episodes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,20 @@ import Layout from "./components/Layout";
 import SeriesDetail from "./pages/Series/SeriesDetail";
 import Episodes from "./pages/Series/Episodes";
 
-
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/series/:id" element={<SeriesDetail />} />
+          <Route path="/series/:seasonId/episodes" element={<Episodes />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 
