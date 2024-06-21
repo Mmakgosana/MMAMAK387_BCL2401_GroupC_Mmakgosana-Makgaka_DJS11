582f674480273c59ffddb2c40cb2751c5d99befa
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-21 11:01:41 +0200

    Implement routing for series details and episodes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Series from "./pages/Series/Series";
+import Layout from "./components/Layout";
+import SeriesDetail from "./pages/Series/SeriesDetail";
+import Episodes from "./pages/Series/Episodes";
 
 
-export default function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
-}
 
 
