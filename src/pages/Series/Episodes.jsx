70b67c4cd7f40b61cfb814a995cf84398179067c
Mmakgosana <makgakammakgosana@gmail.com>
2024-06-21 10:23:02 +0200

    I have used useEffect to fetch the podcasts API

--- src/pages/Series/Episodes.jsx
+++ src/pages/Series/Episodes.jsx
@@ -1,3 +1,17 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+export default function Episodes() {
+    const { seasonId } = useParams();
+    const [episodes, setEpisodes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://podcast-api.netlify.app/id/${seasonId}`)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
